1b4c906fe829d2e248e12c4cfd512a03a24b60e5
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-07-30 12:58:54 +0600

    common item check example done and the best solution without nested loop is always hashmap

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,22 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting the Contains-Common-Item");
 
-        char[] hasCommonItemArray1 = {'a', 'b', 'c', 'x'};
-        char[] hasCommonItemArray2 = {'z', 'y', 'i'};
 
-        char[] hasNoCommonItemArray1 = {'a', 'b', 'c', 'x'};
-        char[] hasNoCommonItemArray2 = {'z', 'y', 'x'};
+        //----------Contains Common Item Check-----------
 
-        ContainsCommonItem callingContainsCommonItem = new ContainsCommonItem();
+//        System.out.println("Starting the Contains-Common-Item");
+//
+//        char[] hasCommonItemArray1 = {'a', 'b', 'c', 'x'};
+//        char[] hasCommonItemArray2 = {'z', 'y', 'i'};
+//
+//        char[] hasNoCommonItemArray1 = {'a', 'b', 'c', 'x'};
+//        char[] hasNoCommonItemArray2 = {'z', 'y', 'x'};
+//
+//        ContainsCommonItem callingContainsCommonItem = new ContainsCommonItem();
 
-        //looping through all the items and it's time complexity it O(n^2)
+//        looping through all the items and it's time complexity it O(n^2)
+
 //        boolean hasCommonItem = callingContainsCommonItem.containsCommonItem(hasCommonItemArray1, hasCommonItemArray2);
 //        boolean hasNoCommonItem = callingContainsCommonItem.containsCommonItem(hasNoCommonItemArray1, hasNoCommonItemArray2);
 //
@@ -23,16 +28,18 @@ public class Main {
 
 
         //converting array to hashmap
-        Map<Character, Integer> commonItemArray1 = callingContainsCommonItem.arrayToHashMap(hasCommonItemArray1);
-        Map<Character, Integer> commonItemArray2 = callingContainsCommonItem.arrayToHashMap(hasCommonItemArray2);
-        Map<Character, Integer> noCommonItemArray1 = callingContainsCommonItem.arrayToHashMap(hasNoCommonItemArray1);
-        Map<Character, Integer> noCommonItemArray2 = callingContainsCommonItem.arrayToHashMap(hasNoCommonItemArray2);
+//        Map<Character, Integer> commonItemArray1 = callingContainsCommonItem.arrayToHashMap(hasCommonItemArray1);
+//        Map<Character, Integer> commonItemArray2 = callingContainsCommonItem.arrayToHashMap(hasCommonItemArray2);
+//        Map<Character, Integer> noCommonItemArray1 = callingContainsCommonItem.arrayToHashMap(hasNoCommonItemArray1);
+//        Map<Character, Integer> noCommonItemArray2 = callingContainsCommonItem.arrayToHashMap(hasNoCommonItemArray2);
+//
+//        boolean hasCommonItemHashMap = callingContainsCommonItem.checkingTwoArraysByHashMap(commonItemArray1, commonItemArray2);
+//        boolean hasNoCommonItemHashMap =callingContainsCommonItem.checkingTwoArraysByHashMap(noCommonItemArray1, noCommonItemArray2);
+//
+//        System.out.println("Has Common Item so it is returning -> "+hasCommonItemHashMap);
+//        System.out.println("Has No Common Item so it is returning -> "+hasNoCommonItemHashMap);
 
-        boolean hasCommonItemHashMap = callingContainsCommonItem.checkingTwoArraysByHashMap(commonItemArray1, commonItemArray2);
-        boolean hasNoCommonItemHashMap =callingContainsCommonItem.checkingTwoArraysByHashMap(noCommonItemArray1, noCommonItemArray2);
 
-        System.out.println("Has Common Item so it is returning -> "+hasCommonItemHashMap);
-        System.out.println("Has No Common Item so it is returning -> "+hasNoCommonItemHashMap);
 
 
     }
