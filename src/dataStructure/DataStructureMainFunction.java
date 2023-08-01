29c72230b27bcf96a9e62616a799d60f5b480985
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-01 15:26:19 +0600

    modification

--- src/dataStructure/DataStructureMainFunction.java
+++ src/dataStructure/DataStructureMainFunction.java
@@ -4,8 +4,8 @@ public class DataStructureMainFunction {
     public static void main(String[] args) {
         System.out.println("Hello World From Data Structure Main");
 
+        //Reverse Str Array
         ReverseString reverseString = new ReverseString();
-
         System.out.println(reverseString.reverseString("Hi, My name is Emon"));
 
     }
---
+++ src/dataStructure/MergeSortedArray.java
@@ -0,0 +1,11 @@
+package dataStructure;
+
+import java.util.List;
+
+public class MergeSortedArray {
+
+    public List<Integer> mergeSortedArray(int[] intArrayOne, int[] intArrayTwo) {
+        System.out.println(intArrayOne+" "+intArrayTwo);
+    }
+
+}
