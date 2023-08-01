d301d7320b4d9776afa184bd079a8b2ed47bcbe7
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-01 17:38:10 +0600

    Merge sorted array

--- src/dataStructure/DataStructureMainFunction.java
+++ src/dataStructure/DataStructureMainFunction.java
@@ -8,5 +8,12 @@ public class DataStructureMainFunction {
         ReverseString reverseString = new ReverseString();
         System.out.println(reverseString.reverseString("Hi, My name is Emon"));
 
+
+        //MergeSortedTwoArrays
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] firstArray = {1,2,3,4};
+        int[] secondArray = {5,6,7,8};
+        mergeSortedArray.mergeSortedArray(new int[]{0,3,4,3}, new int[]{4,6,30});
+
     }
 }
--- src/dataStructure/MergeSortedArray.java
+++ src/dataStructure/MergeSortedArray.java
@@ -1,11 +1,33 @@
 package dataStructure;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSortedArray {
 
     public List<Integer> mergeSortedArray(int[] intArrayOne, int[] intArrayTwo) {
-        System.out.println(intArrayOne+" "+intArrayTwo);
+
+        List<Integer> sortedArray = new ArrayList<>();
+        int i=0;
+        int j=0;
+
+
+        for (int k = 0; k < intArrayOne.length + intArrayTwo.length; k++) {
+            if (i < intArrayOne.length && (j >= intArrayTwo.length || intArrayOne[i] <= intArrayTwo[j])) {
+                sortedArray.add(intArrayOne[i]);
+                i++;
+            } else if (j < intArrayTwo.length) {
+                sortedArray.add(intArrayTwo[j]);
+                j++;
+            }
+        }
+
+
+        for (i=0 ; i<sortedArray.size() ; i++) {
+            System.out.println(sortedArray.get(i));
+        }
+
+        return null;
     }
 
 }
